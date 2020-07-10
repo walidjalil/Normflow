39195bb0da6b4cb17e3981e765d09b003d8df377
walidjalil <walidaj@kth.se>
2020-07-10 18:03:48 +0200

    changed path to download CelebA

--- training.py
+++ training.py
@@ -20,9 +20,10 @@ model = VAE(in_channels=3, out_channels=32, kernel_size=3, n_latent=100)
 model = model.float()
 
 # ------ Load Data
-dataset = torchvision.datasets.CelebA("/Users/walidajalil/", split='train', transform=transforms.Compose([
+dataset = torchvision.datasets.CelebA("/content/gdrive/My Drive/Normflow", split='train', transform=transforms.Compose([
     transforms.CenterCrop(148), transforms.Resize(64), transforms.ToTensor()]), download=True)
 
+"""
 data_load = DataLoader(dataset, batch_size=64, drop_last=True, shuffle=True)
 
 # ------ Initialize optimizer
@@ -46,3 +47,4 @@ for epoch in range(1):
         # if i == 300:
         # print("Reached", i, "iterations!")
         # break
+"""
