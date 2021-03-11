f9005bf6b35d105d12fcf98a09babead27a9a380
root <walidaj@kth.se>
2021-03-11 18:36:44 +0000

    hej

--- Inference.py
+++ Inference.py
@@ -13,7 +13,7 @@ from Inference_NET import VAE
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torch.autograd import Variable
-from data import *
+#from data import *
 import math
 
 model_PATH = '/home/walid_abduljalil/Normflow/model45.pt'
@@ -22,7 +22,7 @@ mel_save_PATH = '/home/walid_abduljalil/Normflow/reconstructed_mel.pt'
 
 # ------ Initialize model
 model = VAE(in_channels=1, out_channels=32, kernel_size=3, n_latent=128)
-model.load_state_dict(torch.load(model_PATH))
+model.load_state_dict(torch.load(model_PATH)['model_state_dict'])
 model.eval()
 model = model.float()
 model.cuda()
@@ -33,13 +33,16 @@ print(" ")
 
 if not os.path.isdir("/home/walid_abduljalil/Normflow/saved_inference_output"):
     os.makedirs("/home/walid_abduljalil/Normflow/saved_inference_output")
-
+print("hej")
     with torch.no_grad():
 
         for i in range(1):
+            print("hej")
+            x = torch.load('/home/walid_abduljalil/Normflow/data/8542.pt')
+            print(x.shape)
             inf_data_input = torch.load(mel_PATH).cuda()
-
-            inference_loss, inference_d_kl, reconstruction = model(val_data_input)
+            print(inf_data_input.shape)
+            inference_loss, inference_d_kl, reconstruction = model(inf_data_input)
             print(reconstruction)
             print("shape of output: ", reconstruction.shape)
             torch.save(reconstruction, mel_save_PATH)
