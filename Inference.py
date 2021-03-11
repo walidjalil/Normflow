8725f6fcd3bad0bf8cbb24a250e6bcf73f3e6ed3
walidjalil <walidaj@kth.se>
2021-03-11 18:38:16 +0100

    yepp

--- Inference.py
+++ Inference.py
@@ -1,87 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
-VAE Class
+Created on Tue Jul  7 02:23:00 2020
 
 @author: walidajalil
 """
 
+import os
+import sys
 import torch
-import numpy as np
-import torch.nn as nn
-from loss import *
+from vanilla_VAE import VAE
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torch.autograd import Variable
+from data import *
+import math
 
+writer = SummaryWriter()
+writer2 = SummaryWriter()
 
-class VAE(nn.Module):
+# ------ Initialize model
+model = VAE(in_channels=1, out_channels=32, kernel_size=3, n_latent=256)
+model.train()
+model = model.float()
+model.cuda()
 
-    def encoder(self, in_channels, out_channels, kernel_size):
-        encode = nn.Sequential(nn.Conv2d(kernel_size=kernel_size, in_channels=in_channels, out_channels=out_channels,
-                                         padding=1, stride=2),
-                               nn.BatchNorm2d(out_channels),
-                               nn.LeakyReLU())
-        return encode
+# ------ Initialize optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=1.0)
+print(" ")
+print("Beginning training now:")
+print(" ")
+model.train()
+if not os.path.isdir("/home/walid_abduljalil/Normflow/saved models"):
+    os.makedirs("/home/walid_abduljalil/Normflow/saved models")
 
-    def decoder(self, in_channels, out_channels, kernel_size):
-        decode = nn.Sequential(nn.ConvTranspose2d(kernel_size=kernel_size, in_channels=in_channels,
-                                                  out_channels=out_channels, padding=1, stride=2, output_padding=1),
-                               nn.BatchNorm2d(out_channels), nn.LeakyReLU())
-        return decode
+epoch_loss_list = []
+epoch_d_kl_list = []
+epoch_val_loss_list = []
+epoch_val_d_kl_list = []
+for epoch in range(71):
+    iteration_loss_list = []
+    iteration_d_kl_list = []
 
-    def create_last_layer(self, in_channels, out_channels, kernel_size):
-        last_layer = nn.Sequential(nn.ConvTranspose2d(kernel_size=kernel_size, in_channels=out_channels,
-                                                      out_channels=out_channels, padding=1, stride=2, output_padding=1),
-                                   nn.BatchNorm2d(out_channels), nn.LeakyReLU(),
-                                   nn.Conv2d(kernel_size=kernel_size, in_channels=out_channels, out_channels=in_channels
-                                             , padding=1))  # , nn.Tanh())
-        return last_layer
+    val_iteration_loss_list = []
+    val_iteration_d_kl_list = []
+    model.train()
+    for i, batch in enumerate(train_loader):
+        data_input = Variable(batch).cuda()
+        optimizer.zero_grad()
 
-    def reparam_trick(self, mu, log_var):
-        norm = torch.randn(mu.size()).cuda()
-        standard_dev = torch.exp(0.5 * log_var)
+        loss_output, d_kl = model(data_input)
+        loss_output.backward()
+        iteration_loss_list.append(loss_output.item())
+        iteration_d_kl_list.append(d_kl.item())
 
-        return norm * standard_dev + mu
+        optimizer.step()
+        scheduler.step()
 
-    def __init__(self, in_channels=1, out_channels=32, kernel_size=3, n_latent=None):
-        super(VAE, self).__init__()
-        super(VAE, self).__init__()
+    epoch_loss_list.append(np.mean(iteration_loss_list))
+    epoch_d_kl_list.append(np.mean(iteration_d_kl_list))
+    writer.add_scalar("Loss/train", np.mean(iteration_loss_list), epoch)
+    print("train loss: ", np.mean(iteration_loss_list))
 
-        self.e1 = self.encoder(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
-        self.e2 = self.encoder(in_channels=out_channels, out_channels=out_channels * 2, kernel_size=kernel_size)
-        self.e3 = self.encoder(in_channels=out_channels * 2, out_channels=out_channels * 4, kernel_size=kernel_size)
-        self.e4 = self.encoder(in_channels=out_channels * 4, out_channels=out_channels * 8, kernel_size=kernel_size)
-        self.e5 = self.encoder(in_channels=out_channels * 8, out_channels=out_channels * 16, kernel_size=kernel_size)
+    with torch.no_grad():
+        model.eval()
+        for i, val_batch in enumerate(validation_loader):
 
-        self.mu = nn.Linear(out_channels * 400, n_latent)
-        self.var = nn.Linear(out_channels * 400, n_latent)
+            val_data_input = Variable(val_batch).cuda()
 
-        self.decoder_input = nn.Linear(n_latent, out_channels * 400)
+            val_loss_output, val_d_kl = model(val_data_input)
 
-        self.d1 = self.decoder(in_channels=out_channels * 16, out_channels=out_channels * 8, kernel_size=kernel_size)
-        self.d2 = self.decoder(in_channels=out_channels * 8, out_channels=out_channels * 4, kernel_size=kernel_size)
-        self.d3 = self.decoder(in_channels=out_channels * 4, out_channels=out_channels * 2, kernel_size=kernel_size)
-        self.d4 = self.decoder(in_channels=out_channels * 2, out_channels=out_channels, kernel_size=kernel_size)
+            val_iteration_loss_list.append(val_loss_output.item())
+            val_iteration_d_kl_list.append(val_d_kl.item())
 
-        self.last_layer = self.create_last_layer(kernel_size=kernel_size, in_channels=in_channels,
-                                                 out_channels=out_channels)
+        epoch_val_loss_list.append(np.mean(val_iteration_loss_list))
+        epoch_val_d_kl_list.append(np.mean(val_iteration_d_kl_list))
+        writer2.add_scalar("Loss/val", np.mean(val_iteration_loss_list), epoch)
+        print("val loss: ", np.mean(val_iteration_loss_list))
+        print("-------------------------------------------------")
 
-    def forward(self, x):
-        encoding1 = self.e1(x)
-        encoding2 = self.e2(encoding1)
-        encoding3 = self.e3(encoding2)
-        encoding4 = self.e4(encoding3)
-        # encoding5 = self.e5(encoding4)
-        flat = torch.flatten(encoding4, start_dim=1)
-        mu = self.mu(flat)
-        log_var = self.var(flat)
-
-        z = self.reparam_trick(mu, log_var)
-
-        d_input = self.decoder_input(z)
-        d_input = d_input.view(-1, 256, 5, 10)
-        decoding2 = self.d2(d_input)
-        decoding3 = self.d3(decoding2)
-        decoding4 = self.d4(decoding3)
-
-        output = self.last_layer(decoding4)
-
-        loss, d_kl = get_loss(mu, log_var, gt_images=x, reconstructions=output.permute(0, 1, 3, 2))
-
-        return loss, d_kl, reconstruction
+    if epoch % 5 == 0:
+        save_prefix = os.path.join("/home/walid_abduljalil/Normflow/saved models")
+        path = "/home/walid_abduljalil/Normflow/saved models/model" + str(epoch) + ".pt"
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': epoch_loss_list, 'D_KL': epoch_d_kl_list,
+        }, path)
